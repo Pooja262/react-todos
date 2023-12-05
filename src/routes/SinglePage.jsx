ad903129e5cf211433e73fabb7166b1d1c4f441e
Pooja262 <65730714+Pooja262@users.noreply.github.com>
2023-12-05 08:01:16 +0530

    Update SinglePage.jsx

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -10,7 +10,7 @@ const aboutData = [
     slug: "about-developer",
     title: "About the developer",
     description:
-      "Ibas Majid founded ibaslogic.com to experiment with new web features and write actionable guides. Follow Ibas on Twitter @ibaslogic to learn modern web development",
+      "Pooja Ponnam developed a todos-app to experiment with new web features. Follow Pooja on LinkedIn @poojaponnam for more updates",
   },
 ];
 const SinglePage = () => {
